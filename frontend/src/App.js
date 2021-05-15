4be4520581da015b7d3a5934a96e7a7f34c6bf07
Christopher <cyoun@chapman.edu>
2021-05-15 14:29:29 -0700

    setup mui theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {ThemeProvider} from '@material-ui/core/styles'
+import CSSBaseline from '@material-ui/core/CssBaseline'
 import Dashboard from './pages/Dashboard/Dashboard'
+
+import theme from './constants/theme'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/dashboard' component={Dashboard} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CSSBaseline />
+      <Router>
+        <Switch>
+          <Route exact path='/dashboard' component={Dashboard} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/constants/theme.js
@@ -0,0 +1,13 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+
+
+const theme = createMuiTheme({
+  palette: {
+  },
+  typography: {
+    fontFamily: '"Josefin Sans",sans-serif'
+  },
+})
+
+
+export default theme
