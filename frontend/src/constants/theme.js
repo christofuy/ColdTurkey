2c0707e7e139662cac69fdcf82e585a7370a89a4
Christopher <cyoun@chapman.edu>
2021-05-15 17:06:41 -0700

    edit mui theme

--- frontend/src/constants/theme.js
+++ frontend/src/constants/theme.js
@@ -3,9 +3,37 @@ import {createMuiTheme} from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
   palette: {
+    text: {
+      primary: '#444444',
+      secondary: '#ADADAD'
+    },
+
   },
   typography: {
-    fontFamily: '"Josefin Sans",sans-serif'
+    fontFamily: '"Josefin Sans",sans-serif',
+    h1: {
+      fontSize: 'clamp(2.25rem,5vw,3rem)',
+      fontWeight: 600,
+      lineHeight: 1.2
+    },
+    h2: {
+      fontSize: 'clamp(1.5rem,4vw,2rem)',
+      fontWeight: 600,
+      lineHeight: 1.2
+    },
+    h3: {
+      fontSize: 'clamp(1.125rem,3vw,1.5rem)',
+      fontWeight: 600,
+      lineHeight: 1.2
+    },
+    h4: {
+      fontSize: 'clamp(0.85rem,3vw,1.125rem)',
+      lineHeight: 1.2
+    },
+    body1: {
+      fontSize: 'clamp(0.8rem,2vw,1rem)',
+      lineHeight: 2
+    },
   },
 })
 
