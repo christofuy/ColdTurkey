10fc4eacb403852123578457a05e9f0ddc3c6c3f
Christopher <cyoun@chapman.edu>
2021-05-15 20:08:16 -0700

    add multiple months to Calendar

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,18 +1,56 @@
 import Typography from '../Typography/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import Week from './Week'
 import useCalendar from '../../hooks/useCalendar'
 import {getWeeks} from '../../util/calendarFxns'
 import './calendar.scss'
 
 const day_labels = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+const n_to_month = {
+  0: 'Jan',
+  1: 'Feb',
+  2: 'Mar',
+  3: 'Apr',
+  4: 'May',
+  5: 'Jun',
+  6: 'Jul',
+  7: 'Aug',
+  8: 'Sep',
+  9: 'Oct',
+  10: 'Nov',
+  11: 'Dec'
+}
 
 const Calendar = () => {
-  const {month, year} = useCalendar()
+  const {month, setMonth, year, setYear} = useCalendar()
   const weeks = getWeeks(month, year)
+
+  const changeMonth = (month) => () => {
+    if (month < 0) {
+      setYear(year - 1)
+      month += 12
+    } else if (month > 11) {
+      setYear(year + 1)
+      month %= 12
+    }
+    setMonth(month)
+  }
 
   return (
     <div className='calendar'>
-      <Typography variant='h2'>Progress</Typography>
+      <div className='calendar__header flex jc-sb ai-center'>
+        <Typography variant='h2'>{n_to_month[month]} {year}</Typography>
+        <div className='buttons'>
+          <IconButton onClick={changeMonth(month - 1)}>
+            <NavigateBeforeIcon />
+          </IconButton>
+          <IconButton onClick={changeMonth(month + 1)}>
+            <NavigateNextIcon />
+          </IconButton>
+        </div>
+      </div>
       <div className='labels flex'>
         {day_labels.map(day => (
           <Typography variant='body1' color='textPrimary' nonresp key={day}>
@@ -25,7 +63,7 @@ const Calendar = () => {
           <Week
             start={start}
             month={month}
-            key={`week-${start.getDate()}`}
+            key={`week-${start}`}
           />
         ))}
       </div>
