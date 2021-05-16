d3c83f1b9b4deaa911a317ab11eb30e9b7850802
Christopher <cyoun@chapman.edu>
2021-05-15 20:06:50 -0700

    add CheckIn component

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,15 +1,19 @@
 import Container from '@material-ui/core/Container'
 import Card from '@material-ui/core/Card'
 import Calendar from '../../components/Calendar/Calendar'
+import CheckIn from '../../components/CheckIn/CheckIn'
 import {CalendarProvider} from '../../hooks/useCalendar'
 
 const Dashboard = () => {
   return (
     <CalendarProvider>
       <Container maxWidth='lg'>
-        <div className='flex'>
+        <div className='flex ai-fstart'>
           <Card>
             <Calendar />
+          </Card>
+          <Card>
+            <CheckIn />
           </Card>
         </div>
       </Container>
